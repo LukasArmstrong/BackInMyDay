cb569ead540a561d31f4fba05ad423f30d50a1a8
Lukas Armstrong <lucas.J.Armstrong@gmail.com>
2021-11-09 22:33:02 -0500

    Fixed issue 001 & Close button of iss. 003

--- public/assests/js/site.js
+++ public/assests/js/site.js
@@ -1,57 +1,42 @@
 $(document).ready(function() {
-  $("#splash_search").keypress(function(e){
-    if (e.which == '13'){
-      $(".Splash-screen").slideUp();
-    }
-  });
-
-  $('#UploadBtn').click(function() {
-    $('#uploadModalCenter').modal('show')
-  });
-
-  $('.close_modal').click(function() {
-    $('#uploadModalCenter').modal('hide')
-  });
-
-  function initMap() {
-    const myLatLng = { lat: 38.212852184793654,  lng: -85.76085961907903 };
-    const myLatLng1 = { lat: 25.363, lng: -131.044 };
-    const myLatLng2 = { lat: -29.363, lng: 31.044 };
-    const latlnng = [myLatLng, myLatLng1, myLatLng2];
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      mapId: "25beecf3843296ec",
-      center: myLatLng,
-    });
-    for (let i = 0; i < latlnng.length; i++) {
-      new google.maps.Marker({
-        position: latlnng[i],
-        map,
-        title: "Hello World!",
-      });
-    }
-  };
-
-  let imagesPreview = function(input, placeToInsertImagePreview) {
-    if (input.files) {
-      let filesAmount = input.files.length;
-      for (i = 0; i < filesAmount; i++) {
-        let reader = new FileReader();
-        reader.onload = function(event) {
-          $($.parseHTML("<img>"))
-            .attr("src", event.target.result)
-            .attr("height",200)
-            .appendTo(placeToInsertImagePreview);
-        };
-        reader.readAsDataURL(input.files[i]);
-      }
-    }
-  };
   $("#input-files").on("change", function() {
     imagesPreview(this, "div.preview-images");
   });
 
 });
+
+$("#splash_search").keypress(function(e){
+  if (e.which == '13'){
+    $(".Splash-screen").slideUp();
+  }
+});
+
+$('.close_modal').click(function() {
+  $('#uploadModalCenter').modal('hide');
+});
+
+
+$('#UploadBtn').click(function() {
+  $('#uploadModalCenter').modal('show');
+});
+
+
+
+let imagesPreview = function(input, placeToInsertImagePreview) {
+  if (input.files) {
+    let filesAmount = input.files.length;
+    for (i = 0; i < filesAmount; i++) {
+      let reader = new FileReader();
+      reader.onload = function(event) {
+        $($.parseHTML("<img>"))
+          .attr("src", event.target.result)
+          .attr("height",200)
+          .appendTo(placeToInsertImagePreview);
+      };
+      reader.readAsDataURL(input.files[i]);
+    }
+  }
+};
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
@@ -84,3 +69,21 @@ function checkCookie() {
      setCookie("visited-BIMD", true, 30);
   }
 }
+function initMap() {
+  const myLatLng = { lat: 38.212852184793654,  lng: -85.76085961907903 };
+  const myLatLng1 = { lat: 25.363, lng: -131.044 };
+  const myLatLng2 = { lat: -29.363, lng: 31.044 };
+  const latlnng = [myLatLng, myLatLng1, myLatLng2];
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    mapId: "25beecf3843296ec",
+    center: myLatLng,
+  });
+  for (let i = 0; i < latlnng.length; i++) {
+    new google.maps.Marker({
+      position: latlnng[i],
+      map,
+      title: "Hello World!",
+    });
+  }
+};
